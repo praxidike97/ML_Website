2a3112ab653c8df5291e628e4710027a49402bdf
Pascal Grafe <dev@pascalgrafe.net>
2021-05-03 01:04:53 +0200

    Ajax

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import torch
 import numpy as np
 from models.VAE import VAE
 import io
+import os
 import base64
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -16,11 +17,14 @@ app = Flask(__name__)
 def machine_learning():
     return render_template("machine_learning.html")
 
-@app.route("/machine_learning/vae", methods=['POST', 'GET'])
-def vae():
+@app.route("/machine_learning/vae/get_image", methods=['POST', 'GET'])
+def vae_get_image():
     z = np.random.normal(size=(1, 2))
     latent_dim01 = z[0, 0]
     latent_dim02 = z[0, 1]
+
+    print("Called!!!")
+    print(request.get_json())
     if request.method == 'POST':
         latent_dim01 = request.form['latent_dim01']
         latent_dim02 = request.form['latent_dim02']
@@ -33,7 +37,6 @@ def vae():
     model = VAE(2, 1)
     model.load_state_dict(torch.load("models/vae_model_weights.pth", map_location=torch.device('cpu')))
     model.eval()
-
     samples = model.decode(z)
     samples = samples.detach().numpy()
 
@@ -52,7 +55,15 @@ def vae():
     pngImageB64String = "data:image/png;base64,"
     pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-    return render_template("vae.html", image=pngImageB64String, latent_dim01=latent_dim01, latent_dim02=latent_dim02)
+    print("Before return!!")
+    #return pngImageB64String, latent_dim01, latent_dim02
+    return jsonify({'image': pngImageB64String, 'latent_dim01': latent_dim01, 'latent_dim02': latent_dim02, 'success': True})
+
+@app.route("/machine_learning/vae", methods=['POST', 'GET'])
+def vae():
+    print("Faaaaaalsch!!")
+    vae_result = vae_get_image().get_json()
+    return render_template("vae.html", image=vae_result['image'], latent_dim01=vae_result['latent_dim01'], latent_dim02=vae_result['latent_dim02'])
 
 @app.route("/machine_learning/style_transfer")
 def style_transfer():
