fbf6837cc4c9fd6ebcb50af8dc26513bc0ca00c8
fabian <fabian@fabians-Mac-mini.local>
2021-05-02 22:27:00 +0200

    New commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,13 +16,25 @@ app = Flask(__name__)
 def machine_learning():
     return render_template("machine_learning.html")
 
-@app.route("/machine_learning/vae")
+@app.route("/machine_learning/vae", methods=['POST', 'GET'])
 def vae():
+    z = np.random.normal(size=(1, 2))
+    latent_dim01 = z[0, 0]
+    latent_dim02 = z[0, 1]
+    if request.method == 'POST':
+        latent_dim01 = request.form['latent_dim01']
+        latent_dim02 = request.form['latent_dim02']
+        print(latent_dim01)
+        print(latent_dim02)
+        z = np.array([[latent_dim01, latent_dim02]])
+
+    z = z.astype(np.float32)
+    z = torch.from_numpy(z).to("cpu")
     model = VAE(2, 1)
     model.load_state_dict(torch.load("models/vae_model_weights.pth", map_location=torch.device('cpu')))
     model.eval()
 
-    samples = model.sample(num_samples=1)
+    samples = model.decode(z)
     samples = samples.detach().numpy()
 
     # Generate plot
@@ -40,7 +52,7 @@ def vae():
     pngImageB64String = "data:image/png;base64,"
     pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-    return render_template("vae.html", image=pngImageB64String)
+    return render_template("vae.html", image=pngImageB64String, latent_dim01=latent_dim01, latent_dim02=latent_dim02)
 
 @app.route("/machine_learning/style_transfer")
 def style_transfer():
